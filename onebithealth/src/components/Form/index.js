8603d11bc1959413202e46072b3ecb2ba2e5c1ce
Andrey Gomes <andreygdsn@gmail.com>
2025-01-29 09:05:29 -0300

    oneBitHeath design

--- onebithealth/src/components/Form/index.js
+++ onebithealth/src/components/Form/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import ResultImc from "./ResultImc/";
+import styles from "./style";
 
 export default function Form() {
   const [height, setHeight] = useState(null);
@@ -28,23 +29,30 @@ export default function Form() {
   }
 
   return (
-    <View>
-      <View>
-        <Text>Altura</Text>
+    <View style={styles.formContext}>
+      <View style={styles.form}>
+        <Text style={styles.formLabel}>Altura</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setHeight}
           value={height}
           placeholder="Ex: 1.70"
           keyboardType="numeric"
         />
-        <Text>Peso</Text>
+        <Text style={styles.formLabel}>Peso</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setWeight}
           value={weight}
           placeholder="Ex: 80.4"
           keyboardType="numeric"
         />
-        <Button onPress={validationImc} title={textButton} />
+        <TouchableOpacity
+          onPress={validationImc}
+          style={styles.buttonCalculator}
+        >
+          <Text style={styles.textButtonCalculator}>{textButton}</Text>
+        </TouchableOpacity>
       </View>
       <ResultImc messageResultImc={messageImc} resultImc={imc} />
     </View>
