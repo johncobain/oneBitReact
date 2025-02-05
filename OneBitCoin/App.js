04ac5b2014ede2b45ef52328393761439273d663
Andrey Gomes <andreygdsn@gmail.com>
2025-02-05 12:17:53 -0300

    api

--- OneBitCoin/App.js
+++ OneBitCoin/App.js
@@ -13,6 +13,83 @@ import HistoryGraphic from "./src/components/HistoryGraphic";
 import QuotationList from "./src/components/QuotationList";
 import QuotationsItems from "./src/components/QuotationList/QuotationsItems";
 
+function convertTimestamp(timestamp, timezone) {
+  // Calcula o deslocamento de GMT-03:00 em milissegundos
+  const offset = -timezone * 60 * 60 * 1000; // -3 horas em milissegundos
+
+  // Adiciona o deslocamento ao timestamp original para obter o timestamp em GMT
+  const gmtTimestamp = timestamp - offset;
+
+  return gmtTimestamp;
+}
+
+function url(qtdDays) {
+  const date = new Date();
+  let endDate = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    0,
+    0,
+    0
+  ).getTime();
+  endDate = convertTimestamp(endDate, -3);
+  const startDate = endDate - qtdDays * 86400000;
+  return `https://api.coincap.io/v2/assets/bitcoin/history?interval=d1&start=${startDate}&end=${endDate}`;
+}
+
+async function getListCoins(url) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+
+    const json = await response.json();
+
+    const reversedList = json.data
+      .map((item) => {
+        const date = item.date.split("T")[0];
+
+        const formattedDate = date.split("-").reverse().join("/");
+
+        return {
+          data: formattedDate,
+          valor: item.priceUsd,
+        };
+      })
+      .reverse();
+
+    return reversedList;
+  } catch (error) {
+    console.error("Erro ao obter ou processar os dados: ", error);
+    return [];
+  }
+}
+async function getPriceCoinsGraphic(url) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+
+    const json = await response.json();
+
+    const reversedList = json.data
+      .map((item) => {
+        item.priceUsd;
+      })
+      .reverse();
+
+    return reversedList;
+  } catch (error) {
+    console.error("Erro ao obter ou processar os dados: ", error);
+    return [];
+  }
+}
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
